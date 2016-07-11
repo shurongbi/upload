8afd7f646710f072a32fe4a8e5f2cab80b007118
chenwenjie <chenwenjie@gclsi.com>
2016-07-11 20:44:48 +0800

    11

--- src/main/java/com/shurong/upload/controller/UploadController.java
+++ src/main/java/com/shurong/upload/controller/UploadController.java
@@ -3,15 +3,22 @@ package com.shurong.upload.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.shurong.upload.service.ReportDailyService;
+import com.shurong.upload.util.ExcelUtils;
+
 @Controller
 public class UploadController {
-
+	
+	@Autowired
+	private ReportDailyService reportDailyService;
+ 
 	@ResponseBody
 	@RequestMapping(value="/uploadDayDaily")
 	public Map<String, Object> uploadDayDaily(@RequestParam("inputdaygly") MultipartFile inputdaygly)
@@ -21,6 +28,12 @@ public class UploadController {
 		{
 			System.out.println(inputdaygly.getOriginalFilename());
 		}
+		try {
+			reportDailyService.doDailyReport(ExcelUtils.initWorkbook(inputdaygly.getInputStream()));
+		} catch (Exception e) {
+			e.printStackTrace();
+		} 
+		
 		returnMap.put("err.code", "上传成功");
 		return returnMap;
 	}
